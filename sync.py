041beefec1d10f664d2fecebcb6205d6ef28cbf6
Sankalpa Fernando <sankalpafernando2017@gmail.com>
2026-03-24 15:55:33 +0530

    Insert cmd added

--- sync.py
+++ sync.py
@@ -38,8 +38,9 @@ def sync_to_cloud(category, name, price, unit,quantity, img_url,market):
     product_data = {"prod_id":prod_id,"price":price,"date":date.today().isoformat()}
 
 
-    supabase.table("products_history").upsert(product_data, on_conflict=["prod_id", "date"]).execute()
-
+    supabase.table("products_history").insert(
+        product_data
+    ).execute()
     
     print(f"Synced {name} to both databases.")
 
